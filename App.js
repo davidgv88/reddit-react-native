e03e59a304b381b27cfab55b2f9640d89ea19f3a
David Gonzalez <davidgv@gmail.com>
2017-10-09 18:18:36 +0200

    Remove remote request

--- App.js
+++ App.js
@@ -20,25 +20,6 @@ export default class App extends React.Component {
     this.makeRemoteRequestReddit();
   }
 
-  
-  makeRemoteRequest = () => {
-    const { page, seed } = this.state;
-    const url = `https://randomuser.me/api/?seed=${seed}&page=${page}&results=20`;
-    this.setState({ loading: true });
-    fetch(url)
-      .then(res => res.json())
-      .then(res => {
-        this.setState({
-          data: page === 1 ? res.results : [...this.state.data, ...res.results],
-          error: res.error || null,
-          loading: false,
-          refreshing: false
-        });
-      })
-      .catch(error => {
-        this.setState({ error, loading: false });
-      });
-  };
   makeRemoteRequestReddit = () => {
     const url = `https://api.reddit.com/r/programming/new.json`;
     this.setState({ loading: true });
